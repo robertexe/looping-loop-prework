47b46bffd4d237e422f1a2b4ab8dbc24c44aa7f5
Robert Tabares <robtabares92@gmail.com>
2018-04-24 19:54:25 +0000

    Done.

--- looping.rb
+++ looping.rb
@@ -1,5 +1,22 @@
-def looping
-	#your code here
+# def loop_arr (arr)
+#   i = 0
+#   arr.length.times do
+#     puts arr[i]
+#     i+=1
+#   end
+# end
+#
+# loop_arr(2,4,6,8)
+
+array = [22,33,44,55,66]
+
+
+def loop_arr(arr)
+i=0
+  arr.length.times do
+    puts "I have found the Time Machine! #{arr[i]}"
+    i+=1
+  end
 end
 
-#call your method here
+loop_arr(array)
