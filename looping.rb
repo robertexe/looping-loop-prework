c54700959e7b72a13194cda8e92b3da19ab36e71
Robert Tabares <robtabares92@gmail.com>
2018-04-24 19:38:11 +0000

    automatically backed up by learn

--- looping.rb
+++ looping.rb
@@ -1,9 +1,17 @@
-def loop_arr (arr)
-  i = 0
-  arr.length.times do
-    puts arr[i]
-    i+=1
+# def loop_arr (arr)
+#   i = 0
+#   arr.length.times do
+#     puts arr[i]
+#     i+=1
+#   end
+# end
+#
+# loop_arr(2,4,6,8)
+
+def loop
+  loop do
+    puts "hello"
   end
 end
 
-#call your method here
+loop
